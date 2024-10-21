802c17e388b8d1825507fe0ad1f6ec1418a14923
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-21 21:18:49 +0200

    update function with table

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,24 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+    <figure key={pokemon.name}>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
