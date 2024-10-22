ed2959c307fde59ce03189ce2afe2bd44fc9bbe7
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-22 16:20:46 +0200

    type props

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const pokemonList = [
 function App() {
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard name ={pokemonList[0].name} imgSrc = {pokemonList[0].imgSrc} />
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,13 @@
-function PokemonCard({ pokemon }) {
+interface PokemonCardProps {
+  name: string;
+  imgSrc?: string;
+}
+
+function PokemonCard({ name, imgSrc }: PokemonCardProps) {
   return (
-    <figure key={pokemon.name}>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
-      ) : (
-        <p>???</p>
-      )}
-      <figcaption>{pokemon.name}</figcaption>
+    <figure key={name}>
+      {imgSrc ? <img src={imgSrc} alt={name} /> : <p>???</p>}
+      <figcaption>{name}</figcaption>
     </figure>
   );
 }
