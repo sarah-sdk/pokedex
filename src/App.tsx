6764d5bae981c088c094f628e54b9b4c22830672
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-22 16:21:34 +0200

    fix biome issue

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const pokemonList = [
 function App() {
   return (
     <div>
-      <PokemonCard name ={pokemonList[0].name} imgSrc = {pokemonList[0].imgSrc} />
+      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
     </div>
   );
 }
